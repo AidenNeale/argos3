443edead5861a249edd7c3abd35cea5b714fbb3a
AidenNeale <aidenneale@sky.com>
2022-07-25 12:31:31 +0100

    CI Ground Colour Sensor Initial Commit

---
+++ src/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_colour_sensor.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_colour_sensor.cpp>
+ *
+ * @author Aiden Neale - <arn519@york.ac.uk>
+ */
+
+#include "ci_pipuck_ground_colour_sensor.h"
+
+#ifdef ARGOS_WITH_LUA
+#include <argos3/core/wrappers/lua/lua_utility.h>
+#endif
+
+namespace argos {
+
+   /****************************************/
+   /****************************************/
+
+#ifdef ARGOS_WITH_LUA
+  void CCI_PiPuckGroundColourSensor::CreateLuaState(lua_State* pt_lua_state) {
+    CLuaUtility::StartTable(pt_lua_state, "ground_colour_sensors");
+    Visit([pt_lua_state] (const SInterface& s_interface) {
+        CLuaUtility::StartTable(pt_lua_state, s_interface.Label + 1);
+        CLuaUtility::AddToTable(pt_lua_state, "reflected", s_interface.Reflected);
+        CLuaUtility::AddToTable(pt_lua_state, "background", s_interface.Background);
+        CLuaUtility::StartTable(pt_lua_state, "transform");
+        CLuaUtility::AddToTable(pt_lua_state, "position", std::get<CVector3>(s_interface.Configuration));
+        CLuaUtility::AddToTable(pt_lua_state, "orientation", std::get<CQuaternion>(s_interface.Configuration));
+        CLuaUtility::AddToTable(pt_lua_state, "anchor", std::get<std::string>(s_interface.Configuration));
+        CLuaUtility::EndTable(pt_lua_state);
+        CLuaUtility::EndTable(pt_lua_state);
+    });
+    CLuaUtility::EndTable(pt_lua_state);
+  }
+#endif
+
+  /****************************************/
+  /****************************************/
+
+#ifdef ARGOS_WITH_LUA
+  void CCI_PiPuckGroundColourSensor::ReadingsToLuaState(lua_State* pt_lua_state) {
+    lua_getfield(pt_lua_state, -1, "ground_colour_sensors");
+    Visit([pt_lua_state] (const SInterface& s_interface) {
+        lua_pushnumber(pt_lua_state, s_interface.Label + 1);
+        lua_gettable(pt_lua_state, -2);
+        lua_pushstring(pt_lua_state, "reflected");
+        lua_pushnumber(pt_lua_state, s_interface.Reflected);
+        lua_settable(pt_lua_state, -3);
+        lua_pushstring(pt_lua_state, "background");
+        lua_pushnumber(pt_lua_state, s_interface.Background);
+        lua_settable(pt_lua_state, -3);
+        lua_pop(pt_lua_state, 1);
+    });
+    lua_pop(pt_lua_state, 1);
+  }
+#endif
+
+  /****************************************/
+  /****************************************/
+
+  const std::map<UInt8, CCI_PiPuckGroundColourSensor::TConfiguration> CCI_PiPuckGroundColourSensor::MAP_SENSOR_CONFIG = {
+    std::make_pair(0, std::make_tuple("origin", CVector3(0.03,  0.01, 0.002), CQuaternion(CRadians::PI, CVector3::Y), 0.005)),
+    std::make_pair(1, std::make_tuple("origin", CVector3(0.03,  0.0,  0.002), CQuaternion(CRadians::PI, CVector3::Y), 0.005)),
+    std::make_pair(2, std::make_tuple("origin", CVector3(0.03, -0.01, 0.002), CQuaternion(CRadians::PI, CVector3::Y), 0.005)),
+  };
+
+  /****************************************/
+  /****************************************/
+
+
+}
---
+++ src/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_colour_sensor.h
@@ -0,0 +1,64 @@
+/**
+ * @file <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_colour_sensor.h>
+ *
+ * @author Aiden Neale - <arn519@york.ac.uk>
+ */
+
+#ifndef CCI_PIPUCK_GROUND_COLOUR_SENSOR_H
+#define CCI_PIPUCK_GROUND_COLOUR_SENSOR_H
+
+namespace argos {
+  //  class CCI_PiPuckGroundColourSensor;
+}
+
+#include <argos3/core/control_interface/ci_sensor.h>
+#include <argos3/core/utility/math/vector3.h>
+#include <argos3/core/utility/math/quaternion.h>
+
+#include <map>
+#include <tuple>
+#include <functional>
+
+namespace argos {
+
+   class CCI_PiPuckGroundColourSensor : public CCI_Sensor {
+
+   public:
+
+      using TConfiguration = std::tuple<std::string, CVector3, CQuaternion, Real>;
+
+      struct SInterface {
+         /* constructor */
+         SInterface(UInt8 un_label) :
+            Label(un_label),
+            Configuration(MAP_SENSOR_CONFIG.at(un_label)),
+            Reflected(0.0),
+            Background(0.0) {}
+         /* members */
+         const UInt8 Label;
+         const TConfiguration& Configuration;
+         Real Reflected;
+         Real Background;
+      };
+
+   public:
+
+      virtual ~CCI_PiPuckGroundColourSensor() {}
+
+      virtual void Visit(std::function<void(const SInterface&)>) = 0;
+
+#ifdef ARGOS_WITH_LUA
+      virtual void CreateLuaState(lua_State* pt_lua_state);
+
+      virtual void ReadingsToLuaState(lua_State* pt_lua_state);
+#endif
+
+   protected:
+
+      static const std::map<UInt8, TConfiguration> MAP_SENSOR_CONFIG;
+
+   };
+
+}
+
+#endif
