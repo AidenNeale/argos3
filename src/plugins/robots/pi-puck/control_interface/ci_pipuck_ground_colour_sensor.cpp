ceb050df50f8691fbfe0da3ad3bc8f2cdcf9d921
AidenNeale <aidenneale@sky.com>
2022-07-26 13:53:52 +0100

    Commented out Lua functionality to prevent abstract errors caused when removing the methods

--- src/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_colour_sensor.cpp
+++ src/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_colour_sensor.cpp
@@ -17,19 +17,18 @@ namespace argos {
 
 #ifdef ARGOS_WITH_LUA
   void CCI_PiPuckGroundColourSensor::CreateLuaState(lua_State* pt_lua_state) {
-    CLuaUtility::StartTable(pt_lua_state, "ground_colour_sensors");
-    Visit([pt_lua_state] (const SInterface& s_interface) {
-        CLuaUtility::StartTable(pt_lua_state, s_interface.Label + 1);
-        CLuaUtility::AddToTable(pt_lua_state, "reflected", s_interface.Reflected);
-        CLuaUtility::AddToTable(pt_lua_state, "background", s_interface.Background);
-        CLuaUtility::StartTable(pt_lua_state, "transform");
-        CLuaUtility::AddToTable(pt_lua_state, "position", std::get<CVector3>(s_interface.Configuration));
-        CLuaUtility::AddToTable(pt_lua_state, "orientation", std::get<CQuaternion>(s_interface.Configuration));
-        CLuaUtility::AddToTable(pt_lua_state, "anchor", std::get<std::string>(s_interface.Configuration));
-        CLuaUtility::EndTable(pt_lua_state);
-        CLuaUtility::EndTable(pt_lua_state);
-    });
-    CLuaUtility::EndTable(pt_lua_state);
+//     CLuaUtility::StartTable(pt_lua_state, "ground_colour_sensors");
+//     Visit([pt_lua_state] (const SInterface& s_interface) {
+//         CLuaUtility::StartTable(pt_lua_state, s_interface.Label + 1);
+//         CLuaUtility::AddToTable(pt_lua_state, "color", s_interface.cColor);
+//         CLuaUtility::StartTable(pt_lua_state, "transform");
+//         CLuaUtility::AddToTable(pt_lua_state, "position", std::get<CVector3>(s_interface.Configuration));
+//         CLuaUtility::AddToTable(pt_lua_state, "orientation", std::get<CQuaternion>(s_interface.Configuration));
+//         CLuaUtility::AddToTable(pt_lua_state, "anchor", std::get<std::string>(s_interface.Configuration));
+//         CLuaUtility::EndTable(pt_lua_state);
+//         CLuaUtility::EndTable(pt_lua_state);
+//     });
+//     CLuaUtility::EndTable(pt_lua_state);
   }
 #endif
 
@@ -38,19 +37,16 @@ namespace argos {
 
 #ifdef ARGOS_WITH_LUA
   void CCI_PiPuckGroundColourSensor::ReadingsToLuaState(lua_State* pt_lua_state) {
-    lua_getfield(pt_lua_state, -1, "ground_colour_sensors");
-    Visit([pt_lua_state] (const SInterface& s_interface) {
-        lua_pushnumber(pt_lua_state, s_interface.Label + 1);
-        lua_gettable(pt_lua_state, -2);
-        lua_pushstring(pt_lua_state, "reflected");
-        lua_pushnumber(pt_lua_state, s_interface.Reflected);
-        lua_settable(pt_lua_state, -3);
-        lua_pushstring(pt_lua_state, "background");
-        lua_pushnumber(pt_lua_state, s_interface.Background);
-        lua_settable(pt_lua_state, -3);
-        lua_pop(pt_lua_state, 1);
-    });
-    lua_pop(pt_lua_state, 1);
+//     lua_getfield(pt_lua_state, -1, "ground_colour_sensors");
+//     Visit([pt_lua_state] (const SInterface& s_interface) {
+//         lua_pushnumber(pt_lua_state, s_interface.Label + 1);
+//         lua_gettable(pt_lua_state, -2);
+//         lua_pushstring(pt_lua_state, "Color");
+//         lua_pushnumber(pt_lua_state, s_interface.cColor);
+//         lua_settable(pt_lua_state, -3);
+//         lua_pop(pt_lua_state, 1);
+//     });
+//     lua_pop(pt_lua_state, 1);
   }
 #endif
 
